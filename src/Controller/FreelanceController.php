6012af5233fb7d8d547b1e33a9eda94cda925593
Siwar Krizi <siwar.krizi@esprit.tn>
2024-02-11 19:12:24 +0100

    first commit

---
+++ src/Controller/FreelanceController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FreelanceController extends AbstractController
+{
+    #[Route('/freelance', name: 'app_freelance')]
+    public function index(): Response
+    {
+        return $this->render('freelance/index.html.twig', [
+            'controller_name' => 'FreelanceController',
+        ]);
+    }
+}
